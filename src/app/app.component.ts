8485754dd70d030f5299c1977da348430423fccd
blueshark42 <roland.csibrei.jr@gmail.com>
2024-09-11 23:22:54 +0200

    WIP design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import { WeatherComponent } from './components/weather/weather.component';
 import { WeatherService } from './services/weather.service';
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WeatherComponent],
+  imports: [RouterOutlet, WeatherComponent, MatTabGroup, MatTab, RouterLink],
   providers: [WeatherService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'swissre';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './components/weather/weather.component';
+import { WeatherWeeklyComponent } from './components/weather-weekly/weather-weekly.component';
+import { NgModule } from '@angular/core';
+import {HomePageComponent} from "./components/home-page/home-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'daily', component: WeatherComponent },
+  { path: 'weekly', component: WeatherWeeklyComponent },
+  { path: '', component: HomePageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
