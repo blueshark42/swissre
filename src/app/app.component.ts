4aeb0ff2888ff80d9936aa6e9a1dfba31cea27f5
blueshark42 <roland.csibrei.jr@gmail.com>
2024-09-11 17:03:39 +0200

    Weather API Information for 1 day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherComponent } from './components/weather/weather.component';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WeatherComponent],
+  providers: [WeatherService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'swissre';
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// Placeholder, would come from server-side or somewhere secure where we do not leak our API keys :)
+
+export const environment = {
+  production: true,
+  openWeatherApiKey: '260dcf223c2fb08edbfac46f40bfbd19',
+};
