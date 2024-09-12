7d16ccde787b80d69943dec580c19d861ec28d20
blueshark42 <roland.csibrei.jr@gmail.com>
2024-09-12 11:57:28 +0200

    Example footer + Responsivity fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,19 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import { WeatherComponent } from './components/weather/weather.component';
 import { WeatherService } from './services/weather.service';
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WeatherComponent, MatTabGroup, MatTab, RouterLink],
+  imports: [
+    RouterOutlet,
+    WeatherComponent,
+    MatTabGroup,
+    MatTab,
+    RouterLink,
+    FooterComponent,
+  ],
   providers: [WeatherService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
